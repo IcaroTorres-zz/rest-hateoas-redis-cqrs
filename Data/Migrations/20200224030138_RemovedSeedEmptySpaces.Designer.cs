076006c59a8cb2dfdd990c82b93e40a4f3013b67
icarotorres <icaro.stuart@gmail.com>
2020-02-26 13:19:28 -0300

    new migration for seed data updates

--- Data/Migrations/20200224030138_RemovedSeedEmptySpaces.Designer.cs
+++ Data/Migrations/20200224030138_RemovedSeedEmptySpaces.Designer.cs
@@ -4,14 +4,16 @@ using Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data.Migrations
 {
     [DbContext(typeof(EnterpriseContext))]
-    partial class EnterpriseContextModelSnapshot : ModelSnapshot
+    [Migration("20200224030138_RemovedSeedEmptySpaces")]
+    partial class RemovedSeedEmptySpaces
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
