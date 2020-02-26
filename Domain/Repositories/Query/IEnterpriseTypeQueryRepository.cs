38b8ff4adc3ed5634d814dd017d7cac6763278c0
icarotorres <icaro.stuart@gmail.com>
2020-02-26 13:18:11 -0300

    added interface signature for enterpriseTypeQueryRepository

---
+++ Domain/Repositories/Query/IEnterpriseTypeQueryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.DTOs.EnterpriseTypes.Inputs;
+using Domain.DTOs.EnterpriseTypes.Outputs;
+using Domain.Util;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories.Query
+{
+    public interface IEnterpriseTypeQueryRepository
+    {
+        Task<EnterpriseTypeOutput> Get(int id);
+        Task<IReadOnlyList<EnterpriseTypeOutput>> Query(EnterpriseTypeIndexFilterInput filter);
+        Pagination<EnterpriseTypeOutput> Paginate(Pagination<EnterpriseTypeOutput> pagination);
+    }
+}
