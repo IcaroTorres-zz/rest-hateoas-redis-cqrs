c39b5f02d53d85cf5da2c2c741fff3e5625f231c
icarotorres <icaro.stuart@gmail.com>
2020-02-26 13:07:30 -0300

    added command and query controllers for enterpriseType entity

---
+++ API.Enterprise/Controllers/Commands/EnterpriseTypesController.cs
@@ -0,0 +1,46 @@
+﻿using API.Actionttributes;
+using Domain.DTOs.EnterpriseTypes.Inputs;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Services.ApplicationServices;
+
+namespace API.Controllers.Commands
+{
+    [ApiController, Route("api/v2/enterprise-types"), ValidateModelState, UnitOfWork]
+    public class EnterpriseTypesController : ControllerBase
+    {
+        private readonly IEnterpriseTypeFacade _service;
+
+        public EnterpriseTypesController(IEnterpriseTypeFacade service)
+        {
+            _service = service;
+        }
+
+        [HttpPost(Name = "create-enterprise-type")]
+        public IActionResult Post([FromBody] string input)
+        {
+            var enterpriseType = _service.CreateEnterpriseType(input);
+            return Created($"{Request.Path.ToUriComponent()}/{enterpriseType.Id}", enterpriseType);
+        }
+
+        [HttpPut("{id}", Name = "update-enterprise-type")]
+        public IActionResult Put(int id, [FromBody] OverwriteEnterpriseTypeInput input)
+        {
+            return Ok(_service.OverwriteEnterpriseType(id, input));
+        }
+
+        [HttpPatch("{id}", Name = "update-enterprise-type-properties")]
+        public IActionResult Patch(int id, [FromBody] JsonPatchDocument<UpdateEnterpriseTypeInput> input)
+        {
+            return Ok(_service.UpdateEnterpriseType(id, input));
+        }
+
+        [HttpDelete("{id}", Name = "disable-enterprise-type")]
+        public IActionResult Delete(int id)
+        {
+            _service.DeleteEnterpriseType(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ API.Enterprise/Controllers/Queries/EnterpriseTypesController.cs
@@ -0,0 +1,31 @@
+﻿using API.Actionttributes;
+using Domain.DTOs.EnterpriseTypes.Inputs;
+using Domain.Repositories.Query;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers.Queries
+{
+    [ApiController, Route("api/v2/enterprise-types"), CacheResponse(30)]
+    public class EnterpriseTypesController : ControllerBase
+    {
+        private readonly IEnterpriseTypeQueryRepository _queryRepository;
+
+        public EnterpriseTypesController(IEnterpriseTypeQueryRepository queryRepository)
+        {
+            _queryRepository = queryRepository;
+        }
+
+        [HttpGet(Name = "get-enterprise-types")]
+        public async Task<IActionResult> Get([FromQuery] EnterpriseTypeIndexFilterInput input)
+        {
+            return Ok(await _queryRepository.Query(input));
+        }
+
+        [HttpGet("{id}", Name = "get-enterprise-type")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _queryRepository.Get(id));
+        }
+    }
+}
