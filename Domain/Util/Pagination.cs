62e3de200460fbbb30b273e5101fc8a565e3ae04
icarotorres <icaro.stuart@gmail.com>
2020-02-26 23:35:40 -0300

    updated pagination class, to be inherited by filterinput DTOs. added GenerateOrderBy method to generate and restrict the order by clause based on reflected members assigned from request querystring. added extension method AddHeadersFromPagination, to add pagination headers in http response.

--- Domain/Util/Pagination.cs
+++ Domain/Util/Pagination.cs
@@ -1,24 +1,62 @@
 ﻿using Domain.Enums;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace Domain.Util
 {
     public class Pagination<T> where T : class
     {
+        [Description("Pagination-ResponsePage"), FromHeader(Name = "Pagination-RequestPage")]
         public int Page { get; set; } = 1;
-        public int PageLength { get; set; } = 10;
-        public string Term { get; set; } = "";
-        public Orderby Order { get; set; } = Orderby.ascending; //"ascending";
-        public string Column { get; set; } = "Id";
-        public decimal TotalInPage { get; set; } = 0m;
-        public decimal Total { get; set; } = 0m;
+
+        [Description("Pagination-PageLength"), FromHeader(Name = "Pagination-PageLength")]
+        public int PageLength { get; set; } = int.MaxValue;
+
+        public long Skip => (PageLength * (Page - 1));
+
+        [Description("Pagination-Order"), FromHeader(Name = "Pagination-Order")]
+        public Orderby Order { get; set; } = Orderby.ASC;
+
+        [Description("Pagination-OrderBy"), FromHeader(Name = "Pagination-OrderBy")]
+        public string OrderBy { get; set; } = "Id";
+
         public IReadOnlyList<T> Items { get; set; } = new List<T>();
 
-        public long Length { get; set; }
-        public long Start => Items.Count == 0 ? 0 : (PageLength * (Page - 1)) + 1;
-        public long End => Items.Count == 0 ? 0 : Start + (Items.Count - 1);
-        public long Pages => Length == 0 ? 1 : (Length + PageLength - 1) / PageLength;
-        public string TotalInPageAsCurrency => TotalInPage.AsBRL();
-        public string TotalEmMoeda => Total.AsBRL();
+        [Description("Pagination-TotalItems")]
+        public long TotalItems { get; set; } = 0;
+
+        [Description("Pagination-TotalPages")]
+        public long Pages => TotalItems == 0 ? 1 : (TotalItems + PageLength - 1) / PageLength;
+
+        public string GenerateOrderBy()
+        {
+            return GetType()
+                .GetProperties()
+                .Where(propInfo => propInfo.GetCustomAttributes(typeof(FromQueryAttribute), true).Any())
+                .Select(propInfo => propInfo.Name)
+                .First(name => name == OrderBy);
+        }
+    }
+
+    public static class PaginationExtension
+    {
+        public static HttpResponse AddHeadersFromPagination<T>(this HttpResponse response, Pagination<T> pagination) where T : class
+        {
+            var headers = pagination
+                .GetType()
+                .GetProperties()
+                .Select(propInfo => (propInfo.GetCustomAttributes(typeof(DescriptionAttribute), true).SingleOrDefault() is DescriptionAttribute att)
+                    ? new { Key = att.Description, Value = propInfo.GetValue(pagination).ToString() }
+                    : null)
+                .Where(header => header != null)
+                .ToList();
+
+            headers.ForEach(header => response.Headers.Add(header.Key, header.Value));
+
+            return response;
+        }
     }
 }
